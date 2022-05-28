d0282ec32a4f17b252ecd9013b9aec8787a4cf34
Ahmed Al-Qossab <alqossaba@gmail.com>
2022-05-28 15:38:34 -0400

    Workshop-Week4

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ import {
     NavbarToggler,
     Nav,
     NavItem,
-    Form
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import NucampLogo from "../app/assets/img/logo.png";
+import UserLoginForm from '../features/user/UserLoginForm';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -45,6 +45,7 @@ const Header = () => {
                         </NavLink>
                     </NavItem>
                 </Nav>
+                <UserLoginForm />
             </Collapse>
         </Navbar>
     );
